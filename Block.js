18721097d49fa6697c216442e30ee7704ef5d2b3
AshutoshSinghTakur <ashuthakur1718@gmail.com>
2021-07-03 13:58:21 +0530

    down

--- Block.js
+++ Block.js
@@ -12,16 +12,17 @@ class Block{
     }
     display(){
       //console.log(this.body.speed);
-      if(this.body.speed > 3){
+      if(this.body.speed > 5){
+        World.remove(world, this.body);
+
       }
       else{
-        World.remove(world, this.body);
-        
+        var pos= this.body.position;
         push();
         var pos= this.body.position;
         rectMode(CENTER);
         rect(pos.x,pos.y,this.width, this.height);
-        this.Visiblity = this.Visiblity - 5;
+        this.Visiblity = this.Visiblity -1;
         tint(255,this.Visiblity);
         pop();
       } 
